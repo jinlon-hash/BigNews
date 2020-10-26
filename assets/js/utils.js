ac9f49228726574c41b351bb971d424c4d32e159
jinlon-hash <1582619568@qq.com>
2020-10-26 21:33:58 +0800

    改变了结构

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,3 +1,7 @@
 $(function () {
-  options.url = 'http://ajax.frontend.itheima.net' + options.url;
+  $.ajaxPrefilter(function (options) {
+    // console.log(options);
+    // 这个函数中的options选项就包含着$.ajax()函数中的对象数据
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
+  });
 });
