e1bb6673729051e81c5a4c539c4e1fcf0bfc6663
jinlon-hash <1582619568@qq.com>
2020-10-28 23:00:11 +0800

    昵称和邮箱的数据校验

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,4 +1,5 @@
 $(function () {
+  var form = layui.form;
   //第一步：获取用户登陆信息，将用户名渲染到用户表单框里
   $.ajax({
     type: 'get',
@@ -9,13 +10,33 @@ $(function () {
         // $('.myForm input[name=username]').val(msg.data.username);
         // $('.myForm input[name=nickname]').val(msg.data.nickname);
         // $('.myForm input[name=email]').val(msg.data.email);
-        layui.form.val('myForm',msg.data);
+
         //对应的每一项进行渲染  layui的内置模块--表单--表单赋值/取值
+        // layui.form.val('myForm', msg.data);
+
+        form.val('myForm', {
+          username: msg.data.username,
+          nickname: msg.data.nickname,
+          email: msg.data.email,
+        });
       }
     },
   });
 
-  // 第二步：表单的submit事件，收集更改信息，然后完成数据交互进行修改
+  //第二步 ：表单校验
+
+  form.verify({
+    nickname: function (value, item) {
+      //value：表单的值、item：表单的DOM对象
+      if (!new RegExp('^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$').test(value)) {
+        return '昵称不能有特殊字符';
+      }
+      if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+        return "昵称首尾不能出现下划线'_'";
+      }
+    },
+  });
+  //第三步：表单的submit事件，收集更改信息，然后完成数据交互进行修改
   $('.myForm').on('submit', function (e) {
     e.preventDefault();
     console.log('sadf');
