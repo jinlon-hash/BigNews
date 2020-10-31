548cbbb09cba437580742799b5fdc01956e30d95
jinlon-hash <1582619568@qq.com>
2020-10-31 23:58:06 +0800

    完成

--- assets/js/article/article_categroy.js
+++ assets/js/article/article_categroy.js
@@ -9,13 +9,30 @@ $(function () {
       $('.lists').html(artList);
     },
   });
+
+  //添加分类布局
   $('.addCate').on('click', function () {
     layer.open({
       type: 1,
       title: '添加文章分类',
-      area:'500px',
+      area: '500px',
       content: $('#addForm').html(),
       //获取所有子内容
+    });
+
+    //添加文章分类  点击确认添加  添加分类
+    //动态生成的  需要事件委托  因为要收集表单数据 所以注册给form
+    $('body').on('click', '.myForm', function (e) {
+      e.preventDefault();
+      $.ajax({
+        type: 'POST',
+        url: '/my/article/addcates',
+        data: $(this).serialize(),
+        success:function(info){
+          //添加成功后隐藏模态框
+          var close
+        }
+      });
     });
   });
   //卡住
