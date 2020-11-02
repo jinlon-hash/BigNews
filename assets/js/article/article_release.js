158ae7f7d5a80d993fd1abe06da7c7f7f4478719
jinlon-hash <1582619568@qq.com>
2020-11-02 10:23:32 +0800

    头像显示完成，下一步头像的上传

--- assets/js/article/article_release.js
+++ assets/js/article/article_release.js
@@ -1,1 +1,18 @@
-$(function () {});
+$(function () {
+  initEditor();
+  $img = $('#image');
+  var option = {
+    aspectRatio: 400/280, //裁切比例
+    preview: '.img-preview',
+  };
+  $img.cropper(option);
+
+  $('.btn-file').on('click', function () {
+    $('#avatar').click();
+  });
+  var $avatar = $('#avatar');
+  $avatar.on('change', function () {
+    var imgSrc = URL.createObjectURL(this.files[0]);
+    $img.cropper('replace', imgSrc);
+  });
+});
